d2a96822b02d4c36b851100ecd80a39e257a1567
zhangaz1 <zhangaz1@hotmail.com>
2016-12-01 15:42:24 +0800

    revert

--- functionalSql-1.js
+++ functionalSql-1.js
@@ -1,7 +1,6 @@
 Array.prototype.findIndex = function(fn) {
-	for(let i = 0; i < this.length; i++)
-		if(fn(this[i])) return i;
-	return -1;
+  for (let i = 0; i < this.length; i++) if (fn(this[i])) return i;
+  return -1;
 };
 
 function chinify(obj, methodName) {
@@ -31,12 +30,10 @@ function decorate(obj, methodName, wrap) {
 }
 
 function product(a, arr) {
-	if(arr.length == 0) return a;
-	let b = arr[0],
-		res = [];
-	for(let x of a)
-		for(let y of b) res.push(x.concat(y));
-	return product(res, arr.slice(1));
+  if (arr.length==0) return a;
+  let b = arr[0], res = [];
+  for (let x of a) for (let y of b) res.push(x.concat(y));
+  return product(res, arr.slice(1));
 }
 
 var splitor = ',';
@@ -44,52 +41,47 @@ var chainMethods = 'select,from,where,orderBy,groupBy,having'.split(splitor);
 var onceMethods = 'select,from,orderBy,groupBy'.split(splitor);
 
 function query() {
-	let s = {
-			where: [],
-			having: []
-		},
-		q = {
-			select: function(fn) {
-				s.select = fn || (x => x);
-			},
-			from: function(a, ...arr) {
-				s.from = () => arr.length == 0 ? a : product(a.map(x => [x]), arr);
-			},
-			where: function(...fns) {
-				s.where.push(x => fns.some(fn => fn(x)));
-			},
-			orderBy: function(fn) {
-				s.orderBy = fn;
-			},
-			groupBy: function(...fns) {
-				s.groupBy = a => a.reduce((res, row) => {
-					let a = res,
-						b;
-					for(let fn of fns) {
-						let group = fn(row);
-						let i = a.findIndex(x => x[0] == group);
-						if(i < 0) a.push([group, a = []]);
-						else a = a[i][1];
-					}
-					a.push(row);
-					return res;
-				}, []);
-			},
-			having: function(...fns) {
-				s.having.push(x => fns.some(fn => fn(x)));
-			},
-			execute: function() {
-				let res = s.from ? s.from() : [];
-				res = res.filter(x => s.where.every(fn => fn(x)));
-				if(s.groupBy) res = s.groupBy(res);
-				res = res.filter(x => s.having.every(fn => fn(x)));
-				if(s.orderBy) res.sort(s.orderBy);
-				return s.select ? res.map(s.select) : res;
-			}
-		};
+  let s = {  where: [], having: [] },
+      q = {
+        select: function(fn){
+          s.select = fn || (x => x);
+        },
+        from: function(a, ...arr){
+          s.from = () => arr.length == 0 ? a : product(a.map(x=>[x]), arr);
+        },
+        where: function(...fns){
+          s.where.push(x => fns.some(fn => fn(x)));
+        },
+        orderBy: function(fn){
+          s.orderBy = fn;
+        },
+        groupBy: function(...fns){
+          s.groupBy = a => a.reduce((res, row) => {
+            let a = res, b;
+            for(let fn of fns) {
+              let group = fn(row);
+              let i = a.findIndex(x => x[0] == group);
+              if (i<0) a.push([group, a = []]); else a = a[i][1];
+            }
+            a.push(row);
+            return res;
+          }, []);
+        },
+        having: function(...fns){
+          s.having.push(x => fns.some(fn => fn(x)));
+        },
+        execute: function(){
+          let res = s.from ? s.from() : [];
+          res = res.filter(x => s.where.every(fn => fn(x)));
+          if (s.groupBy) res = s.groupBy(res);
+          res = res.filter(x => s.having.every(fn => fn(x)));
+          if (s.orderBy) res.sort(s.orderBy);
+          return s.select ? res.map(s.select) : res;
+        }
+      };
 
 	chainMethods.forEach(methodName => chinify(q, methodName));
 	onceMethods.forEach(methodName => onceify(q, methodName));
 
-	return q;
+  return q;
 }
